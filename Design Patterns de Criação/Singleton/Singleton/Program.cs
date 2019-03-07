f0b188e7985e969b844850d43c8ab8309271cc66
Rodrigo <prof.rodrigogoncalvessantana@gmail.com>
2019-03-07 18:19:01 -0300

    Singleton.

---
+++ Design Patterns de Criação/Singleton/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador_1 = Singleton.GetInstancia;
+            jogador_1.Mensagem("Jogador 1: A bola está comigo no meio do campo.");
+
+            Singleton jogador_2 = Singleton.GetInstancia;
+            jogador_2.Mensagem("Jogador 2: recebeu a bola.");
+
+            Singleton jogador_3 = Singleton.GetInstancia;
+            jogador_3.Mensagem("Jogador 3: recebeu o lançamento na linha de fundo.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design Patterns de Criação/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em Jogo");
+                }
+
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
